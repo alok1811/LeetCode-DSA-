8aec28b442bcefcefc3fd77f6542a7f21fe56655
alok1811 <190379967+alok1811@users.noreply.github.com>
2026-07-12 00:07:35 +0530

    [287] [Time Beats: 5.44%] [Memory Beats: 6.57%] - Leet2Hub

--- 01-Arrays-and-Hashing/287. Find the Duplicate Number/287. Find the Duplicate Number.cpp
+++ 01-Arrays-and-Hashing/287. Find the Duplicate Number/287. Find the Duplicate Number.cpp
@@ -1,1 +1,19 @@
-        unordered_map<int, int> mpp;        for(int i = 0; i < n; i++){            mpp[nums[i]]++;        }        for(int i = 0; i < n; i++){            if(mpp[nums[i]] > 1){                dup = nums[i];            }        }        return dup;    }};
+1class Solution {
+2public:
+3    int findDuplicate(vector<int>& nums) {
+4        int n = nums.size(); int dup;
+5
+6        unordered_map<int, int> mpp;
+7
+8        for(int i = 0; i < n; i++){
+9            mpp[nums[i]]++;
+10        }
+11
+12        for(int i = 0; i < n; i++){
+13            if(mpp[nums[i]] > 1){
+14                dup = nums[i];
+15            }
+16        }
+17        return dup;
+18    }
+19};
