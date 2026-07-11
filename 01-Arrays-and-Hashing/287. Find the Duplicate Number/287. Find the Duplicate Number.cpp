e92667de235e904c9979a436940cd7475d4927b2
alok1811 <190379967+alok1811@users.noreply.github.com>
2026-07-11 22:20:32 +0530

    [287] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/287. Find the Duplicate Number/287. Find the Duplicate Number.cpp
@@ -0,0 +1,1 @@
+        unordered_map<int, int> mpp;        for(int i = 0; i < n; i++){            mpp[nums[i]]++;        }        for(int i = 0; i < n; i++){            if(mpp[nums[i]] > 1){                dup = nums[i];            }        }        return dup;    }};
