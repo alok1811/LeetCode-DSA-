49f08a2934746f64a62cde2b1abe92c442fe2332
alok1811 <190379967+alok1811@users.noreply.github.com>
2026-07-12 14:46:42 +0530

    [287] [Time Beats: 100.00%] [Memory Beats: 90.93%] - Leet2Hub

--- 01-Arrays-and-Hashing/287. Find the Duplicate Number/287. Find the Duplicate Number.cpp
+++ 01-Arrays-and-Hashing/287. Find the Duplicate Number/287. Find the Duplicate Number.cpp
@@ -3,17 +3,23 @@
 3    int findDuplicate(vector<int>& nums) {
 4        int n = nums.size(); int dup;
 5
-6        unordered_map<int, int> mpp;
-7
-8        for(int i = 0; i < n; i++){
-9            mpp[nums[i]]++;
-10        }
-11
-12        for(int i = 0; i < n; i++){
-13            if(mpp[nums[i]] > 1){
-14                dup = nums[i];
-15            }
-16        }
-17        return dup;
-18    }
-19};
+6        int slow = 0;
+7        int fast = 0;
+8
+9        while(fast < n){
+10            slow = nums[slow];
+11            fast = nums[fast];
+12            fast = nums[fast];
+13
+14            if(slow == fast){
+15                slow = 0;
+16                while(slow != fast){
+17                    slow = nums[slow];
+18                    fast = nums[fast];
+19                }
+20                return slow;
+21            }
+22        }
+23        return -1;
+24    }
+25};
